ae89f53f20d09aa94f2c088b7c302d7cfff8b065
utsavsahu12345 <utsavsahu12345@gmail.com>
2026-08-13 16:09:20 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  base: '/Dr.-Lal-Path-Labs-Jajpur/', // <-- Bas ye line add hui hai
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, './src'),
